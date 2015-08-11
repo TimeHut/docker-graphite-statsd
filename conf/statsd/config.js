bae6733d6b2c908144ec0a5366d3333f79bcb2e0
Acen <acenqiu@gmail.com>
2015-08-11 11:50:03 +0800

    disable legacy namespace

--- conf/statsd/config.js
+++ conf/statsd/config.js
@@ -1,6 +1,8 @@
 {
-  "graphiteHost": "127.0.0.1",
-  "graphitePort": 2003,
-  "port": 8125,
-  "flushInterval": 10000
+  graphitePort: 2003
+, graphiteHost: "127.0.0.1"
+, graphite: { legacyNamespace: false }
+, port: 8125
+, flushInterval: 10000
+, backends: [ "./backends/graphite" ]
 }
